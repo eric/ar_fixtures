86c866930881153184c02636d1a8324b1ab1639c
Eric Lindvall <eric@5stops.com>
2008-07-30 20:21:58 -0700

    Allow #to_fixture to take an :include argument.
    This allows for dumping models and their relationships.

--- lib/ar_fixtures.rb
+++ lib/ar_fixtures.rb
@@ -57,17 +57,60 @@ class ActiveRecord::Base
     # Uses existing data in the database.
     #
     # Will be written to +test/fixtures/table_name.yml+. Can be restricted to some number of rows.
-    def to_fixture(limit=nil)
-      opts = {}
-      opts[:limit] = limit if limit
+    def to_fixture(*args)
+      options = args.extract_options!
+      # Work with limit as the first argument
+      limit, = *args
+      
+      options[:limit] = limit if limit
+      
+      records = Hash.new do |h, k|
+        h[k] = {}
+      end
 
-      write_file(File.expand_path("test/fixtures/#{table_name}.yml", RAILS_ROOT),
-      self.find(:all, opts).inject({}) { |hsh, record|
-        hsh.merge("#{table_name.singularize}_#{'%05i' % record.id rescue record.id}" => record.attributes)
-      }.to_yaml(:SortKeys => true))
+      includes = options[:include]
+
+      self.find(:all, options).each do |record|
+        record_name = "#{table_name.singularize}_#{'%05i' % record.id}"
+        records[table_name][record_name] = record.attributes
+        
+        if includes
+          traverse_includes(record, includes, records)
+        end
+      end
+
+      records.each do |current_table_name, records|
+        write_file(File.expand_path("test/fixtures/#{current_table_name}.yml", RAILS_ROOT),
+                   records.to_yaml(:SortKeys => true))
+      end
+
       habtm_to_fixture
     end
-
+    
+    def traverse_includes(target_record, includes, records)
+      return unless target_record
+      
+      case includes
+      when Array
+        includes.each do |child_accessor|
+          traverse_includes(target_record, child_accessor, records)
+        end
+      when Hash
+        includes.each do |child_accessor, children_includes|
+          traverse_includes(target_record.send(child_accessor), children_includes, records)
+        end
+      when String, Symbol
+        Array(target_record).each do |record|
+          Array(record.send(includes)).each do |child_record|
+            record_name = "#{child_record.class.table_name.singularize}_#{'%05i' % child_record.id}"
+            records[child_record.class.table_name][record_name] = child_record.attributes
+          end
+        end
+      else
+        raise "Unknown includes type: #{includes.inspect}"
+      end
+    end
+    
     # Write the habtm association table
     def habtm_to_fixture
       joins = self.reflect_on_all_associations.select { |j|
